741794eeedac44a88633fb311ce3e3bcb143345c
Bretton Wade <Bretton.Wade@gmail.com>
2016-05-20 21:45:56 +0100

    links, for future reference

--- src/main/java/com/brettonw/resolver/package-info.java
+++ src/main/java/com/brettonw/resolver/package-info.java
@@ -1,5 +1,9 @@
 /**
  * Provides an all-in-one implementation of dependency resolution on Maven repositories using the
  * Aether library.
+ * See Also:    https://github.com/apache/maven
+ *              https://github.com/eclipse/aether-demo
+ *              https://github.com/eclipse/aether-core
+ *
  */
 package com.brettonw.resolver;
